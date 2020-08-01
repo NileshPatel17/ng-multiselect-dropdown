bf67fe2b9a9da77f3d3407dd2b8065b094930842
Robson Liebke <liebke@media-interactive.de>
2020-08-01 21:43:49 +0200

    fix import to support angular 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { TabsModule, ButtonsModule } from 'ngx-bootstrap';
-import { NgMultiSelectDropDownModule } from '../ng-multiselect-dropdown/src';
-// import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
-
-import { SelectSectionComponent } from './components/select-section';
-import { SampleSectionComponent } from './components/sample-section.component';
-import { SingleDemoComponent } from './components/select/single-demo';
-import { MultipleDemoComponent } from './components/select/multiple-demo';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ShCodeViewer } from '../code-viewer/code-viewer.module';
-
+import { NgMultiSelectDropDownModule } from '../ng-multiselect-dropdown/src';
 import { AppComponent } from './app.component';
+import { SampleSectionComponent } from './components/sample-section.component';
+// import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { SelectSectionComponent } from './components/select-section';
+import { MultipleDemoComponent } from './components/select/multiple-demo';
+import { SingleDemoComponent } from './components/select/single-demo';
+
+
 
 @NgModule({
   declarations: [SelectSectionComponent, SampleSectionComponent, SingleDemoComponent, MultipleDemoComponent, AppComponent],
@@ -28,4 +28,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
